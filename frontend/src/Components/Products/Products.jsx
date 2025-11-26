8a25859085b23d5654dc59538f7a73ec9460fb0e
Alirezak77 <kangavari9@gmail.com>
2025-11-26 06:16:22 -0800

    header of products

--- frontend/src/Components/Products/Products.jsx
+++ frontend/src/Components/Products/Products.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export default function Products() {
   return (
-    <section className='product bg-product-img bg-no-repeat h-[900px]'></section>
-  )
+    <>
+    <svg className='hidden'>
+        <symbol>
+            <svg id="aeeow-left" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+</svg>
+
+        </symbol>
+    </svg>
+    <section className="product bg-product-img bg-no-repeat h-[900px]">
+      <div className="container">
+        <div className="flex items-center justify-between mt-10">
+          <div className="font-laleZar flex flex-col gap-y-2">
+            <h3 className="font-bold text-2xl">جدید ترین محصولات</h3>
+            <p className="text-xl">فراوری شده با دان قهوه</p>
+          </div>
+          <div className="flex items-center text-orange-300">
+            <a href="">مشاهده همه مجصولات</a>
+            <svg className="w-6 h-6">
+                <use href="#arrow-left"></use>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </section>
+    </>
+  );
 }
